211b86eb0b7fa28d911b90fb7e65e0685a458419
Oskar Weber <s189424@student.pg.edu.pl>
2023-03-07 22:13:05 +0100

    x

--- kryptografia_02/main.py
+++ kryptografia_02/main.py
@@ -3,6 +3,13 @@ import string
 import os
 
 
+def dict_key_from_value(l_dict: dict, target_value):
+    keys = [key for key, value in l_dict.items() if value == target_value]
+    if keys:
+        return keys[0]
+    return None
+
+
 def read_file(l_file_name: str) -> list:
     with open(l_file_name, 'r', encoding='UTF-8') as l_file:
         return l_file.readlines()
@@ -41,7 +48,7 @@ def get_freq_dict(l_text: (str, list)) -> dict[chr, float]:
     return l_freq_dict
 
 
-def freq_decrypt(l_alphabet: (str, list), l_todecrypt_filename: str, l_freq_filename: str) -> str:
+def freq_decrypt(l_alphabet: (str, list), l_todecrypt_filename: str, l_freq_filename: str) -> None:
     l_text = read_file(l_todecrypt_filename)[0]
     l_letters_freq = read_file(l_freq_filename)
     l_todecrypt_freq = letter_frequency_analysis(l_alphabet, l_text)
@@ -50,9 +57,10 @@ def freq_decrypt(l_alphabet: (str, list), l_todecrypt_filename: str, l_freq_file
     for l_char, l_char_freq in l_language_freq.items():
         for l_char_toreplace, l_count_freq in l_todecrypt_freq.items():
             if round(abs(l_char_freq - l_count_freq[1]), 3) < 0.7:
-                if l_char_toreplace not in list(l_replace_map.keys()) and l_char not in list(l_replace_map.values()):
+                if l_char_toreplace not in list(l_replace_map.keys()):
                     l_replace_map[l_char_toreplace] = (l_char, round(abs(l_char_freq - l_count_freq[1]), 3))
-                elif round(abs(l_char_freq - l_count_freq[1]), 3) < l_replace_map[l_char_toreplace][1] and l_char not in list(l_replace_map.values()):
+                elif round(abs(l_char_freq - l_count_freq[1]), 3) < l_replace_map[l_char_toreplace][1]:
+                    l_replace_map[dict_key_from_value(l_replace_map, l_replace_map[l_char_toreplace])] = (dict_key_from_value(l_replace_map, l_replace_map[l_char_toreplace]), 100)
                     l_replace_map[l_char_toreplace] = (l_char, round(abs(l_char_freq - l_count_freq[1]), 3))
     l_text = ''.join([l_replace_map[char][0] if char in list(l_replace_map.keys()) else char for char in l_text])
     write_file('replaced.txt', l_text)
